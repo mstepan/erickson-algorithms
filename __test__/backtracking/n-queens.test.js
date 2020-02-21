2a84dddb999280d713176e2a803de934678d81ec
Maksym Stepanenko <maksym.stepanenko@opower.com>
2020-02-21 11:15:17 +0200

    unit tests added for N-queens problem

--- __test__/backtracking/n-queens.test.js
+++ __test__/backtracking/n-queens.test.js
@@ -1,3 +1,9 @@
-test("first test", ()=>{
-    expect("some value").toMatch("some value");
+const queens = require("../../lib/backtracking/n-queens");
+
+test("check placement for 4x4 board", () => {
+    expect(queens.findAllQueensPlacement(4)).toStrictEqual([[1, 3, 0, 2], [2, 0, 3, 1]]);
+});
+
+test("check placement for 8x8 board", () => {
+    expect(queens.findAllQueensPlacement(8).length).toBe(92);
 });
