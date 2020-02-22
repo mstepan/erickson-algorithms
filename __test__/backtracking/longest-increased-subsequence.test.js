1287add6679450acead16a0e4d054e8df8581ac6
Maksym Stepanenko <maksym.stepanenko@opower.com>
2020-02-22 14:24:44 +0200

    alternative solution for LIS using backtracking added

--- __test__/backtracking/longest-increased-subsequence.test.js
+++ __test__/backtracking/longest-increased-subsequence.test.js
@@ -2,17 +2,22 @@ const lis = require("../../lib/backtracking/longest-increased-subsequence");
 
 
 test("LIS normal case", () => {
-    expect(lis.findLongestIncreasedSub([3, 1, 4, 5])).toStrictEqual([1, 4, 5]);
+    expect(lis.longestIncreasedSubLength([3, 1, 4, 5])).toBe(3);
+    expect(lis.longestIncreasedSubLengthAlternative([3, 1, 4, 5])).toBe(3);
 
-    expect(lis.findLongestIncreasedSub([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6]))
-        .toStrictEqual([1, 2, 3, 5, 7, 8]);
+    expect(lis.longestIncreasedSubLength([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6]))
+        .toBe(6);
+    expect(lis.longestIncreasedSubLengthAlternative([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6]))
+        .toBe(6);
 
 });
 
 test("LIS with single element should return single element", () => {
-    expect(lis.findLongestIncreasedSub([3])).toStrictEqual([3]);
+    expect(lis.longestIncreasedSubLength([3])).toBe(1);
+    expect(lis.longestIncreasedSubLengthAlternative([3])).toBe(1);
 });
 
 test("LIS for empty array should return empty sequence", () => {
-    expect(lis.findLongestIncreasedSub([])).toStrictEqual([]);
+    expect(lis.longestIncreasedSubLength([])).toBe(0);
+    expect(lis.longestIncreasedSubLengthAlternative([])).toBe(0);
 });
